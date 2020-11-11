1f7abbca02080ce9b6a2d440ef077cdd7b2eb511
caizongwen <caizongwen@quanyou.com.cn>
2020-11-11 13:53:18 +0800

    项目变更2

---
+++ checkupdatelibrary/src/main/java/com/tomcat/checkupdatelibrary/interfaces/OnDownloadListener.java
@@ -0,0 +1,12 @@
+package com.tomcat.checkupdatelibrary.interfaces;
+
+import java.io.File;
+
+/**
+ * 创建者：TomCat0916
+ * 创建时间：2020/11/9
+ * 功能描述：
+ */
+public interface OnDownloadListener<T extends BaseVersionEntity> {
+    void onDownload(File apk, String url, DownLoadResult downLoadResult, T data);
+}
